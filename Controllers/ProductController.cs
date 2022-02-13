9e3ad614d3cc782eb9eddd405c98570b7ac1d4e9
MdHamidUddin <hamiduddin2018@gmail.com>
2022-02-13 23:24:55 +0600

    Product Add to Cart

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using Product.Models;
+using System.Text.Json;
+using Newtonsoft.Json;
+
+namespace Product.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductEntities dbObj = new ProductEntities();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Products()
+        {
+            var result = dbObj.products.ToList();
+            return View(result);
+        }
+
+        public ActionResult AddToCart(product p)
+        {
+            List<object> Cart = new List<object>();
+            string json;
+            if (Session["cart"] == null)
+            {
+                Cart[]
+                Cart.Add(p);
+                json = new JavaScriptSerializer().Serialize(Cart);
+                Session["cart"] = json;
+            }
+
+            else
+            {
+               // var L = JavaScriptSerializer().Deserialize<List< object >> (Session["cart"]);
+                var model = JsonConvert.DeserializeObject<List<MatrixModel.RootObject>>(json);
+            }
+           
+
+            return View(result);
+        }
+
+
+
+    }
+}
